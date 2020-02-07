ced77337e6162af4a7abb42e20231f7d1b5f8185
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2020-02-07 12:22:14 +0100

    Add files via upload

---
+++ Board.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+class Board {
+  constructor(n, side, starting_point_x, starting_point_y) {
+    this.n = n;
+    this.side = side;
+    this.starting_point_x = starting_point_x;
+    this.starting_point_y = starting_point_y;
+  }
+
+  draw_board() {
+    //Rows
+    for (let i = 0; i < n; i++) {
+      //Columns
+      for (let j = 0; j < n; j++) {
+        if ((i % 2 === 0 && j % 2 === 0) || (i % 2 != 0 && j % 2 != 0)) {
+          ctx.fillRect(
+            this.starting_point_x + j * side,
+            this.starting_point_y + i * side,
+            this.side,
+            this.side
+          );
+        } else {
+          ctx.rect(
+            this.starting_point_x + j * side,
+            this.starting_point_y + i * side,
+            this.side,
+            this.side
+          );
+          ctx.stroke();
+        }
+      }
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let A = new Point(10, 10);
-let ABCD = new Square(A.x, A.y, 100);
+let A_x = 0;
+let A_y = 0;
 
-ABCD.draw_map(2);
+let n = 8;
+let side = canvas.width / n;
+let board = new Board(n, side, A_x, A_y);
+
+board.draw_board();
